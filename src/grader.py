73eeb912721b225c459bcd24d9bbdb3596474bb7
Insop Song <insop.song@gmail.com>
2021-04-05 23:47:03 -0700

    Add traceback to grader.py, h/t Michael Kamfonas

--- src/grader.py
+++ src/grader.py
@@ -3,6 +3,7 @@ import unittest, random, sys, copy, argparse, inspect
 from graderUtil import graded, CourseTestRunner, GradedTestCase
 import numpy as np
 import os
+import traceback
 
 # Import student submission
 import submission
@@ -61,15 +62,8 @@ class DummyModel(object):
     """
 
     def predict(self, partial_parses):
-        out = []
-        for pp in partial_parses:
-            if len(pp.buffer) > 0:
-                out.append("S")
-            elif pp.stack[1] is "left" and len(pp.stack) > 2:
-                out.append("LA")
-            else:
-                out.append("RA")
-        return out
+        return [("RA" if pp.stack[1] is "right" else "LA") if len(pp.buffer) == 0 else "S"
+                for pp in partial_parses]
 
 def test_dependencies(name, deps, ex_deps):
     """Tests the provided dependencies match the expected dependencies"""
@@ -91,9 +85,9 @@ def test_minibatch_parse():
     test_dependencies("minibatch_parse", deps[1],
                       (('ROOT', 'right'), ('arcs', 'only'), ('only', 'again'), ('right', 'arcs')))
     test_dependencies("minibatch_parse", deps[2],
-                      (('ROOT', 'only'), ('only', 'arcs'), ('only', 'left')))
+                      (('only', 'ROOT'), ('only', 'arcs'), ('only', 'left')))
     test_dependencies("minibatch_parse", deps[3],
-                      (('ROOT', 'again'), ('again', 'arcs'), ('again', 'left'), ('again', 'only')))
+                      (('again', 'ROOT'), ('again', 'arcs'), ('again', 'left'), ('again', 'only')))
     print("minibatch_parse test passed!")
 
 
